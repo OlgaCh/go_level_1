cb61da0851e5b02e1c38a8fdec700ad63a5691be
Olga Chebotaryova <olga.chebotaryova@gmail.com>
2021-02-22 21:52:42 +0300

    Prime number. Reduce checks.

--- lesson2/simple.go
+++ lesson2/simple.go
@@ -25,12 +25,18 @@ func PrimeNumber() {
 func isPrime(n int) bool {
 	if n == 1 || n == 0 {
 		return false
-	} else {
-		for i := 2; i < n; i++ {
-			if n%i == 0 {
-				return false
-			}
-		}
+	}
+	if n <= 3 {
 		return true
 	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
 }
